20affc8274038a04df6c8e4ea8767cfeada980b1
Vijayboopathy24 <vijayboopathy@gmail.com>
2024-12-04 19:01:53 +0530

    succesfully Tested login page

--- src/test/java/org/pojo/Pojoclass.java
+++ src/test/java/org/pojo/Pojoclass.java
@@ -3,11 +3,40 @@ package org.pojo;
 import org.base.BaseClass;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Pojoclass extends BaseClass{
 
+public	Pojoclass(){
+	PageFactory.initElements(driver, this);
+}
 	
-	@FindBy(xpath = "//a[text()='Sign In']")
+	public WebElement getSignin() {
+		return signin;
+	}
+	public WebElement getCreateAcc() {
+		return createAcc;
+	}
+	public WebElement getFirstname() {
+		return firstname;
+	}
+	public WebElement getLastname() {
+		return lastname;
+	}
+	public WebElement getEmail() {
+		return email;
+	}
+	public WebElement getPassword() {
+		return password;
+	}
+	public WebElement getShowPass() {
+		return showPass;
+	}
+	public WebElement getSignUp() {
+		return signUp;
+	}
+	
+	@FindBy(xpath = "(//a[@data-link-type='nav-global'])[5]")
 	private WebElement signin;
 	@FindBy(xpath = "//a[@title='Create an Account']")
 	private WebElement createAcc;
